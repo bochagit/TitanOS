92888c55cbd401ee44cbba4667f6ca8bb7ff43ce
bochagit <gocardozo@alumno.unlam.edu.ar>
2026-06-03 00:23:57 -0300

    Add registration UI, Supabase Server and Client. Registration page and form components, basic UI. Supabase Auth configured. Table Users and Gyms created at Supabase.

---
+++ src/components/auth/register-form.tsx
@@ -0,0 +1,99 @@
+'use client'
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Button } from '@/components/ui/button'
+import { useState } from 'react'
+import { createClient } from '@/lib/supabase/client'
+
+export default function RegisterForm() {
+  const supabase = createClient()
+
+  const [fullName, setFullName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [gymName, setGymName] = useState('')
+
+  const handleSubmit = async (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    console.log({ fullName, email, password, gymName })
+
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+    })
+
+    console.log(data)
+    console.log(error)
+  }
+
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-black px-4">
+      <Card className="w-full max-w-md">
+        <CardHeader className="space-y-2 text-center">
+          <h1 className="text-3xl font-bold">TreinoSuite</h1>
+
+          <p className="text-muted-foreground">
+            Tus alumnos organizados.
+            <br />
+            Tu negocio bajo control.
+          </p>
+        </CardHeader>
+
+        <CardContent>
+          <form className="space-y-4" onSubmit={handleSubmit}>
+            <div className="space-y-2">
+              <Label htmlFor="fullName">Nombre completo</Label>
+              <Input
+                id="fullName"
+                placeholder="John Doe"
+                required
+                value={fullName}
+                onChange={e => setFullName(e.target.value)}
+              />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="John@email.com"
+                required
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+              />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="password">Contraseña</Label>
+              <Input
+                id="password"
+                type="password"
+                required
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="gym">Nombre del gimnasio</Label>
+              <Input
+                id="gym"
+                placeholder="Treino Gym"
+                required
+                value={gymName}
+                onChange={e => setGymName(e.target.value)}
+              />
+            </div>
+
+            <Button className="w-full" type="submit">
+              Crear cuenta
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </main>
+  )
+}
